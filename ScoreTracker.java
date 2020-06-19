74c85470b2e0274f856a7383bf7fc501036c73ab
Lektro <kevin.vanderkruk@gmail.com>
2020-06-19 09:53:48 +0200

    Update ScoreTracker.java
    cleaned up the code

--- ScoreTracker.java
+++ ScoreTracker.java
@@ -4,13 +4,11 @@ import java.util.Scanner;
 public class ScoreTracker {
     public int numberOfStudents;
     public String [] scoreValues;
-    // public int [] nbr;
 
     public static void main(String[] args) {
         ScoreTracker st = new ScoreTracker();
         st.getUserInput();
         st.setValuesToArray();
-        //st.doGrading();
     }
 
     private void getUserInput() {
@@ -29,37 +27,11 @@ public class ScoreTracker {
 
     private void setValuesToArray() {
 
-        int array [] = new int[numberOfStudents];
+        int [] array = new int[numberOfStudents];
         int size = array.length;
         for (int i = 0; i < size; i++) {
            array[i] = Integer.parseInt((scoreValues[i]));
         }
-        System.out.println(array[2]);
-
+        System.out.println(Arrays.toString(array));
     }
-/*
-    private void doGrading(){
-        double max = Double.parseDouble(scoreValues[0]);
-        int index = (numberOfStudents);
-        for (int i = 1; i < index; i++) {
-            if (nbr[i] > max){
-                max = nbr[i];
-                index = i;
-                System.out.println(index);
-            }
-        }
-    }
-
- */
 }
-
-
-
-
-
-
-
-
-
-
-
